032641ed7c0eb39cae37fc320878c5eb631212b0
Vítor Rocha <vitor.rocha03@gmail.com>
2022-12-26 12:26:33 +0000

    Add project

---
+++ DogsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DogsApp
+//
+//  Created by Vítor Rocha on 26/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
